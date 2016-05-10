189a9615d3ecc105ed90c9407f25939bd4aa58ea
yingyuk <wuyingyucn@gmail.com>
2016-05-10 09:57:12 +0800

    min

--- server.js
+++ server.js
@@ -2,19 +2,17 @@
 * @Author: Yuk
 * @Date:   2016-05-08 10:04:56
 * @Last Modified by:   Yuk
-* @Last Modified time: 2016-05-08 11:27:58
+* @Last Modified time: 2016-05-10 09:40:37
 */
 
 'use strict';
 var express = require('express');
 var path = require('path');
-var serveStatic = require('serve-static');
 var app = express();
 var port = 5000;
 
-app.use(serveStatic(path.join(__dirname, '/src/')));
+app.use(express.static(__dirname + '/dist'));
 app.get('/',function (req,res) {
-	// res.sendFile('index.html');
 })
 app.listen(port);
 console.log('server listening on port 5000 ');
